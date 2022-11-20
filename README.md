fc648509fb1fa0b43547e62e1460b60a1b4b555a
Owen Pan <opan@ucsd.edu>
2022-11-20 01:31:04 -0800

    Finish Lab 8

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Lab 8 - Starter
+
+1. Manually run them locally before pushing code
+2. No
+3. No, since the "message" feature likely involves numerous components acting together. Unit testing these individual components instead would lead far more detailed results.
+4. Yes, since this is a straightforward small feature that has an easily testable function (given an input, we can easily verify if the feature yields a correct output).
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,82 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+test('valid phone number format 1', () => {
+  expect(functions.isPhoneNumber('123-456-7890')).toBe(true);
+});
+
+test('valid phone number format 2', () => {
+  expect(functions.isPhoneNumber('(123) 456-7890')).toBe(true);
+});
+
+test('invalid phone number format 1', () => {
+  expect(functions.isPhoneNumber('123-4-5')).toBe(false);
+});
+
+test('invalid phone number format 2', () => {
+  expect(functions.isPhoneNumber('abcde')).toBe(false);
+});
+
+test('valid email format 1', () => {
+  expect(functions.isEmail('opan@ucsd.edu')).toBe(true);
+});
+
+test('valid email format 2', () => {
+  expect(functions.isEmail('abcd@efghi.jk')).toBe(true);
+});
+
+test('invalid email format 1', () => {
+  expect(functions.isEmail('opan@ucsd.e')).toBe(false);
+});
+
+test('invalid email format 2', () => {
+  expect(functions.isEmail('opan@123.edu')).toBe(false);
+});
+
+test('valid strong password format 1', () => {
+  expect(functions.isStrongPassword('hunter2')).toBe(true);
+});
+
+test('valid strong password format 2', () => {
+  expect(functions.isStrongPassword('password123')).toBe(true);
+});
+
+test('invalid strong password format 1', () => {
+  expect(functions.isStrongPassword('a')).toBe(false);
+});
+
+test('invalid strong password format 2', () => {
+  expect(functions.isStrongPassword('abcdefghijklmnopqrstuvwxyz')).toBe(false);
+});
+
+test('valid date format 1', () => {
+  expect(functions.isDate('1/1/2000')).toBe(true);
+});
+
+test('valid date format 2', () => {
+  expect(functions.isDate('01/01/2000')).toBe(true);
+});
+
+test('invalid date format 1', () => {
+  expect(functions.isDate('1/1/00')).toBe(false);
+});
+
+test('invalid date format 2', () => {
+  expect(functions.isDate('1-1-2000')).toBe(false);
+});
+
+test('valid hex color format 1', () => {
+  expect(functions.isHexColor('#FFFFFF')).toBe(true);
+});
+
+test('valid hex color format 2', () => {
+  expect(functions.isHexColor('111')).toBe(true);
+});
+
+test('invalid hex color format 1', () => {
+  expect(functions.isHexColor('FFFFFFF')).toBe(false);
+});
+
+test('invalid hex color format 2', () => {
+  expect(functions.isHexColor('#11')).toBe(false);
+});
